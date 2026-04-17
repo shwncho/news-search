01955fa9160252fbdaf1db06628d95764ec902da
shwncho <shwncho4@gmail.com>
2026-04-17 10:11:59 +0900

    create project

---
+++ src/test/kotlin/com/example/newssearch/NewsSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.newssearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
